8b86e2b51b9a552600aff652da765622dd8b22b5
kelar crisp <kelcrisp007@icloud.com>
2020-04-05 22:20:54 -0700

    refatcored the way im calling the server and accessing data

--- sockets/Server.js
+++ sockets/Server.js
@@ -1,17 +1,48 @@
+
 const WebSocketServer = require('ws').Server
-
 const wss = new WebSocketServer({ port: 9090 })
-
 
 const clients = [];
 wss.on('connection', (connection) => {
     clients.push(connection)
 
     connection.on('message', (message) => {
-        console.log(message)
         const data = JSON.parse(message)
-        clients.forEach((client) => {
-            client.send(JSON.stringify(data))
-        })
+        console.log(data)
+        connection.send(JSON.stringify(data));
+
+        // clients.forEach((client) => {
+        //     console.log(JSON.stringify(data))
+        //     client.send(JSON.stringify(data))
+        // })
     })
 })
+
+
+
+
+
+// const WebSocketServer = require('ws').Server
+
+// const wss = new WebSocketServer({ port: 9090 })
+
+
+// const clients = [];
+// let holder = [];
+// wss.on('connection', (connection) => {
+//     clients.push(connection)
+//     // console.log(connection, 'connection ')
+//     connection.on('message', (message) => {
+//         let data = JSON.parse(message)
+//         holder.push({
+//             myUser: data.username.myUser,
+//             message: data.message
+//         })
+//         console.log(holder, 'holder')
+//         // clients.forEach((client) => {
+//         //     client.send(JSON.stringify(data))
+//         // })
+//     })
+//     let dataToSend = JSON.stringify(holder.slice(-1))
+//     connection.send(dataToSend)
+// })
